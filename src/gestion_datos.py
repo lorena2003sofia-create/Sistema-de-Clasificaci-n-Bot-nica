c9bcf3ed72c3b5aceaea4b699b58944423566f32
lorena2003sofia-create <lorena2003sofia@gmail.com>
2026-05-10 20:15:57 -0700

    Create gestion_datos.py

---
+++ src/gestion_datos.py
@@ -0,0 +1,11 @@
+class Archivador:
+    @staticmethod
+    def guardar(plantas, nombre_archivo="herbario.txt"):
+        try:
+            with open(nombre_archivo, "w", encoding="utf-8") as f:
+                for p in plantas:
+                    f.write(f"{p.especie}|{p.nombre_comun}|{p.obtener_cuidados()}\n")
+            return True
+        except Exception as e:
+            print(f"Error: {e}")
+            return False 
