7f9a613861ec1bcecc081850552ff456d834f460
lorena2003sofia-create <lorena2003sofia@gmail.com>
2026-05-14 21:38:34 -0700

    Update gestion_datos.py

--- src/gestion_datos.py
+++ src/gestion_datos.py
@@ -4,7 +4,8 @@ class Archivador:
         try:
             with open(nombre_archivo, "w", encoding="utf-8") as f:
                 for p in plantas:
-                    f.write(f"{p.especie}|{p.nombre_comun}|{p.obtener_cuidados()}\n")
+                    f.write(f"{p.especie} | {p.nombre_comun} | {p.obtener_cuidados()}\n")
             return True
         except Exception as e:
+            print(f"Error al escribir: {e}")
             return False
